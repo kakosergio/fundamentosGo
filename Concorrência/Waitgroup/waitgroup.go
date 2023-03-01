306affe0cfb4be24570e77fb8cefdeee6beb0139
kakosergio <sergioricardort@hotmail.com>
2023-03-01 06:32:35 -0300

    waitGroup.add(4)

--- Concorrência/Waitgroup/waitgroup.go
+++ Concorrência/Waitgroup/waitgroup.go
@@ -10,19 +10,29 @@ func main() {
 	var waitGroup sync.WaitGroup
 
 	// grupo de espera que tem duas goroutines que fazem parte dela
-	waitGroup.Add(2)
+	waitGroup.Add(4)
 
 	go func() {
 		go escrever("Olá Mundo!")
-		waitGroup.Done() // -1
+		waitGroup.Done() // -1 - waitGroup.Add(3)
 	}()
 
 	go func() {
 		escrever("Programando em Go!")
-		waitGroup.Done() // -1 no waitGroup.add(2)
+		waitGroup.Done() // -1 - waitGroup.add(2)
 	}()
 
-	waitGroup.Wait()
+	go func() {
+		go escrever("Eita Preula!")
+		waitGroup.Done() // -1 - waitGroup.add(1)
+	}()
+
+	go func() {
+		escrever("Bora fazer back-end!")
+		waitGroup.Done() // -1 - waitGroup.add(0)
+	}()
+
+	waitGroup.Wait() //  0 - Espera acabar as goroutines que foram indicadas no waitGroup.add(4), até que todas terminem
 }
 
 func escrever(texto string) {
