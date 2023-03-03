34928261349578257904843a2687e25552da80d1
kakosergio <sergioricardort@hotmail.com>
2023-03-03 07:15:57 -0300

    Não tá indo

--- Testes_automatizados/Introducao/enderecos/enderecos.go
+++ Testes_automatizados/Introducao/enderecos/enderecos.go
@@ -2,8 +2,8 @@ package enderecos
 
 import "strings"
 
-// TipoDeEndereco verifica se o endereço tem um tipo válido e o retorna
-func TipoDeEndereco (endereco string) string {
+// TipoEndereco verifica se o endereço tem um tipo válido e o retorna
+func TipoEndereco (endereco string) string {
 	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
 
 	primeiraPalavraDoEndereco := strings.Split(strings.ToLower(endereco), " ")[0]
