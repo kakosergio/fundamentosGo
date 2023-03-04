5ab9b60b0dd5231a0cd1bfebb52f133ef37cc2e3
kakosergio <sergioricardort@hotmail.com>
2023-03-04 20:50:47 -0300

    gambi pra continuar a aula

--- Testes_automatizados/Introducao/introducao.go
+++ Testes_automatizados/Introducao/introducao.go
@@ -1,6 +1,26 @@
 package main
 
-func main (){
-	
+import (
+	"fmt"
+	"strings"
+)
 
+func main (){
+	tipoEndereco := TipoEndereco("Avenida Paulista")
+	fmt.Println(tipoEndereco)
+	
+}
+
+// TipoEndereco verifica se o endereço tem um tipo válido e o retorna
+func TipoEndereco (endereco string) string {
+	tiposValidos := []string{"rua", "avenida", "estrada", "rodovia"}
+
+	primeiraPalavraDoEndereco := strings.Split(strings.ToLower(endereco), " ")[0]
+
+	for _, tipo := range tiposValidos{
+		if tipo == primeiraPalavraDoEndereco{
+			return strings.Title(primeiraPalavraDoEndereco)
+		}
+	}
+	return "Tipo inválido"
 }
